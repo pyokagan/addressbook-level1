65dce84fa5cbacb337d17d825cb2f5ce5a13e514
Brandon <brandonyeoxg@gmail.com>
2016-12-05 14:40:22 +0800

    Edit runtests.bat to work with new FilePathUtil.java

--- test/runtests.bat
+++ test/runtests.bat
@@ -4,10 +4,12 @@ REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM compile the code into the bin folder
-javac  ..\src\seedu\addressbook\Addressbook.java -d ..\bin
+javac  ..\src\seedu\addressbook\util\FilePathUtil.java -d ..\bin\util ..\src\seedu\addressbook\Addressbook.java -d ..\bin
 
 REM run the program, feed commands from input.txt file and redirect the output to the actual.txt
 java -classpath ..\bin seedu.addressbook.AddressBook < input.txt > actual.txt
 
 REM compare the output to the expected output
 FC actual.txt expected.txt
+
+cmd /k
