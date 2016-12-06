0e7abfecd3d6a2b10fb462f5ad37ecc77c633be4
Brandon <brandonyeoxg@gmail.com>
2016-12-06 15:06:18 +0800

    Edit isValidFilePath to a better implementation.

--- src/seedu/addressbook/util/FilePathUtil.java
+++ src/seedu/addressbook/util/FilePathUtil.java
@@ -10,53 +10,40 @@ public class FilePathUtil {
     private static final int EXTENSION_INVALID_INDEX = 0;
 
     /**
-     * Returns true if the given file is acceptable.
-     * The file path is acceptable if it does not contain any reserved characters <>:"/|?*,
-     * has a filename with an extension.
-     * TODO: Implement a more rigorous validity checking.
+     * Returns true if the given file path is acceptable.
+     * The file path is acceptable if it has a folder that exists,
+     * has file name that is acceptable to the operating system eg. does not contain <>:"/|?*.
+     * File name must also contain an extension.
      */
     public static boolean isValidFilePath(String filePath) {
-        return isValidPath(filePath) && isValidFileName(filePath);
-    }
-    
-    /**
-     * Returns true if the path is valid.
-     * Path is valid if there is no reserved characters used, if it is a directory.
-     * If path is not a directory, the path is valid if it's parent folder exists.
-     */
-    private static boolean isValidPath(String filePath) {
         if (filePath == null) {
             return false;
         }
-        File pathToValidate = new File(filePath);
-        if (!pathToValidate.isDirectory()) {
-            return isValidParentFolder(pathToValidate);
-        }
-        return pathToValidate.exists();
-    }
-
-    /**
-     * Returns true if the parent folder is valid.
-     * Parent folder is valid if it exists.
-     */
-    private static boolean isValidParentFolder(File pathToValidate) {
-        File parentFolder = pathToValidate.getParentFile();
-        if (parentFolder == null) {
+        File filePathToValidate = new File(filePath);
+        if (!hasValidFileName(filePathToValidate)) {
             return false;
         }
-        return parentFolder.exists();
+        if(!hasValidFolder(filePathToValidate)) {
+            return false;
+        }
+        return true;
     }
     
     /**
-     * Returns true if the file is valid.
-     * File is valid if it has a name, an extension and no reserved characters <>:"/|?*.
+     * Returns true if the file path has a folder that exists
      */
-    private static boolean isValidFileName(String filePath) {
-        if (filePath == null) {
-            return false;
-        }
+    private static boolean hasValidFolder(File path) {
+        File folderToValidate = path.getAbsoluteFile().getParentFile();
+        return folderToValidate.exists();
+    }
+    
+    /**
+     * Returns true if the file path has a valid file name
+     * File name is valid if it has a name, an extension and no reserved characters <>:"/|?*.
+     */
+    private static boolean hasValidFileName(File filePath) {
         try {
-            File fileNameToValidate = new File(filePath).getCanonicalFile();
+            File fileNameToValidate = new File(filePath.getName()).getCanonicalFile();
             int extensionSeparatorIndex = fileNameToValidate.getName().lastIndexOf(".");
             return extensionSeparatorIndex > EXTENSION_INVALID_INDEX;
         } catch (IOException ioe) {
