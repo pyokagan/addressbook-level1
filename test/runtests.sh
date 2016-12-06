32f3b438f2dd2667e7066a360808b69e8cc002c7
Brandon <brandonyeoxg@gmail.com>
2016-12-06 15:08:15 +0800

    Attempt to remove FilePathUtilTest.java from commits

--- test/runtests.sh
+++ test/runtests.sh
@@ -10,7 +10,7 @@ then
 fi
 
 # compile the code into the bin folder
-javac  ../src/seedu/addressbook/AddressBook.java -d ../bin
+javac  ../src/seedu/addressbook/util/FilePathUtil.java -d ../bin/util ../src/seedu/addressbook/AddressBook.java -d ../bin
 
 # run the program, feed commands from input.txt file and redirect the output to the actual.txt
 java -classpath ../bin seedu.addressbook.AddressBook < input.txt > actual.txt
