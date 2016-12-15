7143599f036498397ff05645ff6f409f936e6fb3
Brandon <brandonyeoxg@gmail.com>
2016-12-15 19:47:21 +0800

    Add comments for runtests.bat and runtests.sh

--- test/runtests.bat
+++ test/runtests.bat
@@ -6,15 +6,18 @@ if not exist ..\bin mkdir ..\bin
 REM compile the code into the bin folder
 javac  ..\src\seedu\addressbook\Addressbook.java -d ..\bin
 
-REM run the program, feed commands from input.txt file and redirect the output to the actual.txt
+REM run the program and pass in invalid file paths
 java -classpath ..\bin seedu.addressbook.AddressBook " " < NUL > actual.txt
 java -classpath ..\bin seedu.addressbook.AddressBook "directoryThatDoesNotExist/valid.filename" < NUL >> actual.txt
 java -classpath ..\bin seedu.addressbook.AddressBook ".noFilename" < NUL >> actual.txt
+REM run the program and pass in valid a file path
 mkdir data
 copy /y NUL data\addressbook.txt
 java -classpath ..\bin seedu.addressbook.AddressBook "data/addressbook.txt" < exitinput.txt >> actual.txt
+REM run the program and pass in a non regular file (a directory with the name data/notRegularFile.txt)
 mkdir data\notRegularFile.txt
 java -classpath ..\bin seedu.addressbook.AddressBook "data/notRegularFile.txt" < NUL >> actual.txt
+REM run the program, feed commands from input.txt file and redirect the output to the actual.txt
 java -classpath ..\bin seedu.addressbook.AddressBook < input.txt >> actual.txt
 
 REM compare the output to the expected output
--- test/runtests.sh
+++ test/runtests.sh
@@ -12,16 +12,20 @@ fi
 # compile the code into the bin folder
 javac  ../src/seedu/addressbook/AddressBook.java -d ../bin
 
-# run the program, feed commands from input.txt file and redirect the output to the actual.txt
+# run the program and pass in invalid file paths
 java -classpath ../bin seedu.addressbook.AddressBook ' ' < /dev/null > actual.txt
 java -classpath ../bin seedu.addressbook.AddressBook 'directoryThatDoesNotExist/valid.filename' < /dev/null >> actual.txt
 java -classpath ../bin seedu.addressbook.AddressBook '.noFilename' < /dev/null >> actual.txt
+# run the program and pass in valid a file path
 mkdir -p data
 touch data/addressbook.txt
 java -classpath ../bin seedu.addressbook.AddressBook 'data/addressbook.txt' < exitinput.txt >> actual.txt
+# run the program and pass in a non regular file (a directory with the name data/notRegularFile.txt)
 mkdir -p data/notRegularFile.txt
 java -classpath ../bin seedu.addressbook.AddressBook 'data/notRegularFile.txt' < /dev/null >> actual.txt
+# run the program, feed commands from input.txt file and redirect the output to the actual.txt
 java -classpath ../bin seedu.addressbook.AddressBook < input.txt >> actual.txt
+
 # compare the output to the expected output
 diff actual.txt expected.txt
 if [ $? -eq 0 ]
